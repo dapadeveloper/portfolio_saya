d0949fa75b7cce8adfba3d33ea7f4b0bd6abf2e6
Dapaweb <dodisetya0890@gmail.com>
2026-01-14 09:17:23 +0700

    update

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ import os
 # =====================
 # KONFIGURASI HALAMAN
 # =====================
-st.set_page_config(page_title="Naufal Daffa | Portfolio", page_icon="📊", layout="wide")
+st.set_page_config(
+    page_title="Naufal Daffa | Portfolio", 
+    page_icon="📊", 
+    layout="wide",
+    initial_sidebar_state="auto" 
+)
 
 # FUNGSI UNTUK MERENDER GAMBAR LOKAL KE HTML (BASE64)
 def get_image_base64(path):
